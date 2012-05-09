8fd275749e38d04cee2179fcd8a00527f44571ff
Pascal <michelpa@gmail.com>
2012-05-09 13:58:01 +0200

    Add configuration for the navbar (thanks to sylwit)

--- Grid/Grid.php
+++ Grid/Grid.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace EPS\JqGridBundle\Grid;
-
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\Query;
@@ -13,7 +12,8 @@ use Doctrine\ORM\Query;
  *
  * @author pascal
  */
-class Grid {
+class Grid
+{
 
     /**
      * @var \Symfony\Component\DependencyInjection\Container
@@ -45,18 +45,21 @@ class Grid {
     private $options;
     private $routeforced;
     private $hideifempty;
+    private $navOptions;
 
     /**
      * @var string
      */
     private $hash;
 
-    public function __construct($container, $paginator) {
+    public function __construct($container, $paginator)
+    {
         $this->container = $container;
 
         $this->router = $container->get('router');
         $this->request = $container->get('request');
-        $this->session = $this->request->getSession();
+        $this->session = $this->request
+                              ->getSession();
         $this->paginator = $paginator;
         $this->em = $container->get('doctrine.orm.entity_manager');
         $this->templating = $container->get('templating');
@@ -66,7 +69,8 @@ class Grid {
         $this->routeforced = '';
         $this->hideifempty = false;
 
-        if ($this->request->isXmlHttpRequest()) {
+        if ($this->request
+                 ->isXmlHttpRequest()) {
             $this->onlyData = true;
         } else {
             $this->onlyData = false;
@@ -79,13 +83,15 @@ class Grid {
         unset($this->routeParameters['_route']);
     }
 
-    public function setSource(QueryBuilder $qb) {
+    public function setSource(QueryBuilder $qb)
+    {
         $this->qb = $qb;
         //generate hash
         $this->createHash();
     }
 
-    public function addColumn($name, $colmodel) {
+    public function addColumn($name, $colmodel)
+    {
         $col = new Column();
         $col->setName($name);
         $col->setColModel($colmodel);
@@ -93,11 +99,13 @@ class Grid {
         return $col;
     }
 
-    public function getColumns() {
+    public function getColumns()
+    {
         return $this->columns;
     }
 
-    public function getColumnsNames() {
+    public function getColumnsNames()
+    {
         $tabColNames = array();
         foreach ($this->columns as $c) {
             $tabColNames[] = '\'' . $c->getName() . '\'';
@@ -106,7 +114,8 @@ class Grid {
         return $colnames;
     }
 
-    public function getColumnsColModel() {
+    public function getColumnsColModel()
+    {
         $tabcolmodels = array();
 
         foreach ($this->columns as $c) {
@@ -118,82 +127,116 @@ class Grid {
         return $colmodels;
     }
 
-    public function setName($name) {
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function setHideIfEmpty($hideifempty) {
+    public function setHideIfEmpty($hideifempty)
+    {
         $this->hideifempty = $hideifempty;
     }
 
-    public function getHideIfEmpty() {
+    public function getHideIfEmpty()
+    {
         return $this->hideifempty;
     }
 
-    public function setCaption($caption) {
+    public function setCaption($caption)
+    {
         $this->caption = $caption;
     }
 
-    public function getCaption() {
+    public function getCaption()
+    {
         return $this->caption;
     }
 
-    public function getRouteUrl() {
+    public function getRouteUrl()
+    {
         if ($this->routeforced != '') {
             return $this->routeforced;
         } else {
-            return $this->router->generate($this->request->get('_route'));
+            return $this->router
+                        ->generate($this->request
+                                        ->get('_route'));
         }
     }
 
-    public function setRouteForced($route) {
+    public function setRouteForced($route)
+    {
         $this->routeforced = $route;
     }
 
-    public function isOnlyData() {
+    public function isOnlyData()
+    {
         return $this->onlyData;
     }
 
-    public function createHash() {
-        $this->hash = 'grid_' . md5($this->request->get('_controller') . $this->getName());
-        $this->session->set($this->getHash(), 'Y');
+    public function createHash()
+    {
+        $this->hash = 'grid_' . md5($this->request
+                                         ->get('_controller') . $this->getName());
+        $this->session
+                ->set($this->getHash(), 'Y');
     }
 
-    public function getHash() {
+    public function getHash()
+    {
         return $this->hash;
     }
 
-    public function render() {
+    public function render()
+    {
         if ($this->isOnlyData()) {
             $response = new Response();
             $response->setContent(json_encode($this->getData()));
-            $response->headers->set('Content-Type', 'application/json');
+            $response->headers
+                     ->set('Content-Type', 'application/json');
             return $response;
         } else {
-            return array('grid' => $this);
+            return array(
+                'grid' => $this
+            );
         }
     }
 
-    public function getData() {
-        if ($this->session->get($this->getHash()) == 'Y') {
+    public function getData()
+    {
+        if ($this->session
+                 ->get($this->getHash()) == 'Y') {
 
-            $page = $this->request->query->get('page');
-            $limit = $this->request->query->get('rows');
-            $sidx = $this->request->query->get('sidx');
-            $sord = $this->request->query->get('sord');
-            $search = $this->request->query->get('_search');
+            $page = $this->request
+                         ->query
+                         ->get('page');
+            $limit = $this->request
+                          ->query
+                          ->get('rows');
+            $sidx = $this->request
+                         ->query
+                         ->get('sidx');
+            $sord = $this->request
+                         ->query
+                         ->get('sord');
+            $search = $this->request
+                           ->query
+                           ->get('_search');
 
             if ($sidx != '') {
-                $this->qb->orderBy($sidx, $sord);
+                $this->qb
+                        ->orderBy($sidx, $sord);
             }
 
             if ($search == 'true') {
                 $paramnumber = 1;
-                $filters = json_decode($this->request->query->get('filters'), true);
+                $filters = json_decode($this->request
+                                            ->query
+                                            ->get('filters'), true);
                 $rules = $filters['rules'];
 
                 if ($rules) {
@@ -204,14 +247,19 @@ class Grid {
                                 if ($c->getFieldFormatter() == 'date') {
                                     $tmp = explode("/", $rule['data']);
                                     $date = $tmp[2] . "-" . $tmp[1] . "-" . $tmp[0];
-                                    $this->qb->andWhere($c->getFieldIndex() . ' LIKE \'%' . $date . '%\'');
+                                    $this->qb
+                                            ->andWhere($c->getFieldIndex() . ' LIKE \'%' . $date . '%\'');
                                 } elseif ($c->getFieldHaving()) {
-                                    $this->qb->having($c->getFieldHaving() . " =  ?$paramnumber");
-                                    $this->qb->setParameter($paramnumber, $rule['data']);
+                                    $this->qb
+                                            ->having($c->getFieldHaving() . " =  ?$paramnumber");
+                                    $this->qb
+                                            ->setParameter($paramnumber, $rule['data']);
                                     $paramnumber++;
                                 } else {
-                                    $this->qb->andWhere($c->getFieldIndex() . " LIKE  ?$paramnumber");
-                                    $this->qb->setParameter($paramnumber, '%' . $rule['data'] . '%');
+                                    $this->qb
+                                            ->andWhere($c->getFieldIndex() . " LIKE  ?$paramnumber");
+                                    $this->qb
+                                            ->setParameter($paramnumber, '%' . $rule['data'] . '%');
                                     $paramnumber++;
                                 }
                             }
@@ -220,9 +268,11 @@ class Grid {
                 }
             }
 
-            $pagination = $this->paginator->paginate(
-                    $this->qb->getQuery()->setHydrationMode(Query::HYDRATE_ARRAY), $page/* page number */, $limit/* limit per page */
-            );
+            $pagination = $this->paginator
+                               ->paginate($this->qb
+                                               ->getQuery()
+                                               ->setHydrationMode(Query::HYDRATE_ARRAY), $page/* page number */, $limit/* limit per page */
+                               );
 
             $nbRec = $pagination->getTotalItemCount();
 
@@ -237,7 +287,6 @@ class Grid {
             $response['total'] = $total_pages;
             $response['records'] = $nbRec;
 
-
             foreach ($pagination as $key => $item) {
                 $row = $item;
 
@@ -248,7 +297,10 @@ class Grid {
                     } elseif ($c->getFieldValue()) {
                         $val[] = $c->getFieldValue();
                     } elseif ($c->getFieldTwig()) {
-                        $val[] = $this->templating->render($c->getFieldTwig(), array('ligne' => $row));
+                        $val[] = $this->templating
+                                      ->render($c->getFieldTwig(), array(
+                                      'ligne' => $row
+                                      ));
                     } else {
                         $val[] = ' ';
                     }
@@ -263,23 +315,49 @@ class Grid {
         }
     }
 
-    public function setDefaultOptions() {
+    public function setDefaultOptions()
+    {
         $this->options = array(
-            'height' => '100%',
-            'rowNum' => 10,
-            'rowList' => array(10, 20, 30),
-            'datatype' => 'json',
-            'viewrecords' => true,
+            'height' => '100%', 'rowNum' => 10, 'rowList' => array(
+                10, 20, 30
+            ), 'datatype' => 'json', 'viewrecords' => true,
+        );
+
+        $this->navOptions = array(
+            'view' => false, 'search' => false, 'edit' => false, 'add' => false, 'del' => false,
         );
     }
 
-    public function setOptions(array $options) {
+    public function setOptions(array $options)
+    {
         foreach ($options as $k => $v) {
             $this->options[$k] = $options[$k];
         }
     }
 
-    public function getOptions($json = true) {
+    public function setNavOptions(array $options)
+    {
+        foreach ($options as $k => $v) {
+            $this->navOptions[$k] = $options[$k];
+        }
+    }
+
+    public function getNavOptions($json = true)
+    {
+        if ($json) {
+            $opts = json_encode($this->navOptions);
+            $opts = substr($opts, 1);
+            $opts = substr($opts, 0, strlen($opts) - 1);
+            $opts = $opts . ', ';
+            return $opts;
+        } else {
+            return $this->navOptions;
+        }
+
+    }
+
+    public function getOptions($json = true)
+    {
         if ($json) {
             $opts = json_encode($this->options);
             $opts = substr($opts, 1);
@@ -292,8 +370,10 @@ class Grid {
         }
     }
 
-    public function getCulture() {
-        if ($l = $this->request->get('_locale') != '') {
+    public function getCulture()
+    {
+        if ($l = $this->request
+                      ->get('_locale') != '') {
             return $l;
         } else {
             return \Locale::getDefault();
